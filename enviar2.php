fdf9de7e1eff02317bc8d99eb33274f89a752b83
belkixrequejo <belkix.requejo@pucese.edu.ec>
2019-10-31 17:21:16 -0500

    correccion correo

--- enviar2.php
+++ enviar2.php
@@ -4,20 +4,16 @@ $mail = $_POST['email'];
 $tema = $_POST['tema'];
 $mensajes = $_POST['mensaje'];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Mensaje: " . $mensajes" \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'maritzarequejo2208@gmail.com';
+$para = 'belkix.requejo@pucese.edu.ec';
 $asunto = 'Mensaje de mi sitio web';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
-
+mail($para, $asunto, $mensaje);
 header("Location:contact.html");
+
 ?>
