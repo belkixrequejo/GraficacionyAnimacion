1430b2fd4cc3b67f4ddc4926850bab531dc65301
belkixrequejo <belkix.requejo@pucese.edu.ec>
2019-10-31 17:46:16 -0500

    correccion correo1

--- enviar2.php
+++ enviar2.php
@@ -1,14 +1,11 @@
 <?php
 $nombre = $_POST['nombre'];
-$mail = $_POST['email'];
+$email = $_POST['email'];
 $tema = $_POST['tema'];
 $mensajes = $_POST['mensaje'];
 
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $mensajes" \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+
 
 $para = 'belkix.requejo@pucese.edu.ec';
 $asunto = 'Mensaje de mi sitio web';
